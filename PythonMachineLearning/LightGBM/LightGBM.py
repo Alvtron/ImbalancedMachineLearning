88f9e447682a9fa51dccc41b2c8374b0b3fc313b
Thomas Angeland <thomas.angeland@gmail.com>
2019-02-23 20:45:40 +0100

    Added optimal parameters to LightGBM.

--- PythonMachineLearning/LightGBM/LightGBM.py
+++ PythonMachineLearning/LightGBM/LightGBM.py
@@ -16,17 +16,17 @@ model_parameters = {
     'boosting_type': ['gbdt'], # ‘gbdt’, traditional Gradient Boosting Decision Tree. ‘dart’, Dropouts meet Multiple Additive Regression Trees. ‘goss’, Gradient-based One-Side Sampling. ‘rf’, Random Forest.
     'n_jobs': [-1],
     'num_class': [10],
-    'n_estimators': [300], # 100, Number of boosted trees to fit.
+    'n_estimators': [800], # 100, Number of boosted trees to fit.
     'num_leaves': [60], # 31, Maximum tree leaves for base learners.
     'max_depth': [10], # -1, Maximum tree depth for base learners, -1 means no limit.
-    'learning_rate': [0.8], # 0.1, Boosting learning rate.
+    'learning_rate': [0.4], # 0.1, Boosting learning rate.
     'min_split_gain': [0], # 0, Minimum loss reduction required to make a further partition on a leaf node of the tree.
     'min_child_samples': [5], # 20, Minimum number of data needed in a child (leaf).
     'subsample': [1], # 1, Subsample ratio of the training instance.
     'subsample_freq': [0], # 0, Frequence of subsample, <=0 means no enable.
     'colsample_bytree': [1], # 1, Subsample ratio of columns when constructing each tree.
     'reg_alpha': [0], # 0, L1 regularization term on weights.
-    'reg_lambda': [1], # 0, L2 regularization term on weights.
+    'reg_lambda': [0], # 0, L2 regularization term on weights.
     'objective': ['multiclass'],
     'metric': ['multi_logloss']
     }
